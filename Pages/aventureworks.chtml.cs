21f05bb267aa946f084f13f4d8d0d23253fc3f94
Jody Ford <jodyford@microsoft.com>
2024-09-12 21:50:47 -0400

    second commit

--- Pages/aventureworks.chtml.cs
+++ Pages/aventureworks.chtml.cs
@@ -1,51 +1,61 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;  
-using System;  
-using System.Collections.Generic;  
-using System.Data.SqlClient;  
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient; // Updated namespace
+using System.Threading.Tasks;
 
-public class AdventureWorksModel : PageModel  
-{  
-    public List<Person> People { get; set; } = new List<Person>();  
+public class AdventureWorksModel : PageModel
+{
+    private readonly IConfiguration _configuration;
 
-    public void OnGet()  
-    {  
-        string connectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");  
+    public AdventureWorksModel(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
-        if (string.IsNullOrEmpty(connectionString))  
-        {  
-            throw new Exception("Connection string not found in environment variables.");  
-        }  
+    public List<Person> People { get; set; } = new List<Person>();
 
-        try  
-        {  
-            using (SqlConnection connection = new SqlConnection(connectionString))  
-            {  
-                connection.Open();  
-                string query = "SELECT TOP 10 FirstName, LastName FROM Person.Person";  
+    public async Task OnGetAsync()
+    {
+        string? connectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
 
-                using (SqlCommand command = new SqlCommand(query, connection))  
-                using (SqlDataReader reader = command.ExecuteReader())  
-                {  
-                    while (reader.Read())  
-                    {  
-                        People.Add(new Person  
-                        {  
-                            FirstName = reader["FirstName"].ToString(),  
-                            LastName = reader["LastName"].ToString()  
-                        });  
-                    }  
-                }  
-            }  
-        }  
-        catch (Exception ex)  
-        {  
-            throw new Exception($"An error occurred: {ex.Message}");  
-        }  
-    }  
-}  
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new Exception("Connection string not found in configuration.");
+        }
 
-public class Person  
-{  
-    public string FirstName { get; set; }  
-    public string LastName { get; set; }  
-}  
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "SELECT TOP 10 FirstName, LastName FROM Person.Person";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        People.Add(new Person
+                        {
+                            FirstName = reader["FirstName"]?.ToString() ?? string.Empty,
+                            LastName = reader["LastName"]?.ToString() ?? string.Empty
+                        });
+                    }
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            // Log exception (ex) here
+            throw new Exception("An error occurred while querying the database.", ex);
+        }
+    }
+}
+
+public class Person
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
